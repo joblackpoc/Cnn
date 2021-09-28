31731c6ba754a188257de2efbea1c09f1cd0f746
joblackpoc <duopoc@gmail.com>
2021-09-28 23:39:56 +0700

    First commit

---
+++ main/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+def Home(request):
+    return render(request, 'main/home.html')
+
+def register(request):
+    if request.method == 'POST':
+      form = UserRegisterForm(request.POST)
+      if form.is_valid():
+          form.save()
+          username = form.cleaned_data.get('username')
+          messages.success(request, f'Your account {username} has bee created !')
+          return redirect('login')
+    else:
+      form = UserRegisterForm()
+    return render(request, 'main/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+      u_form = UserUpdateForm(request.POST, instance=request.user)
+      p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+      if u_form.is_valid() and p_form.is_valid():
+        u_form.save()
+        p_form.save()
+        messages.success(request, f'Your accout has been Update')
+        return redirect('profile')
+    else:
+      u_form = UserUpdateForm(instance=request.user)
+      p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+      'u_form': u_form,
+      'p_form': p_form
+    }
+    return render(request, 'main/profile.html', context)
